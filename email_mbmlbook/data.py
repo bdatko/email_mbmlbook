5c9cf8cff49296079742327b3866cf6f70f51dd8
bdatko <ben.datko@gmail.com>
2022-03-06 20:38:50 -0500

    refactor(email_mbmlbook/data.py): remove features on construction
    features can be determined automatically from the objml file

--- email_mbmlbook/data.py
+++ email_mbmlbook/data.py
@@ -13,16 +13,29 @@ import untangle
 class FeatureSet:
     xml: str
     datasets: list
-    features: list
 
     def __post_init__(self):
         self.tree = untangle.parse(self.xml)
         self.base = "InputsCollection.Inputs.Inputs"
+        self.features = self._validate_features()
         self.columns = ["user", "dataset"] + self.features + ["repliedTo"]
         self.source = {
             dataset: self.base + f".{dataset}.Instances.Instance"
             for dataset in self.datasets
         }
+
+    def _validate_features(self):
+        _features = []
+        for user_input in attrgetter(self.base)(self.tree):
+            features = [
+                item._name for item in user_input.FeatureSet.Features.get_elements()
+            ]
+            _features.append(features)
+
+        if any(element != _features[0] for element in _features):
+            raise ValueError("FeatureSet are different accross users")
+
+        return _features[0]
 
     def to_pandas(self):
         data = []
