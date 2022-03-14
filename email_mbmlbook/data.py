4c0c19fcdc04c9c8ce5352cb36f513f2fa43f179
bdatko <ben.datko@gmail.com>
2022-03-13 21:05:27 -0400

    fix(email_mbmlbook/data.py): add varying parsing if there is either missing, expected, or multiple values
    Datasets could have either missing values or categories with multiple values. Need to check for either the three conditoins then parse the categories. Required additional helper functions.

--- email_mbmlbook/data.py
+++ email_mbmlbook/data.py
@@ -3,11 +3,14 @@ Classes for Data Objects
 """
 
 from dataclasses import dataclass
+from itertools import zip_longest
 from operator import attrgetter
+from typing import Union
 
+import numpy as np
 import pandas as pd
 import untangle
-
+from more_itertools import peekable
 from untangle import Element
 
 
@@ -59,6 +62,18 @@ class FeatureSet:
 
         return _maps
 
+    def _get_value(
+        self, user: str, feature: str, ref: str, double: Union[int, float]
+    ) -> Union[str, float, int]:
+        """
+        Returns either the double or mapped ref after determining if the feature
+        has a boolan or categorical (cat > 2) domain
+        """
+        if len(self.categories[user][feature]) > 1:
+            return self.categories[user][feature][ref]
+        elif len(self.categories[user][feature]) == 1:
+            return double
+
     def _parse_values(self, user: str, instance: Element) -> list:
         """
         Parse values from an instance
@@ -79,11 +94,57 @@ class FeatureSet:
 
         return values
 
-    def _parse_missing_values():
-        pass
+    def _parse_missing_values(self, user: str, instance: Element) -> list:
+        """
+        Parse values from an instance
+        when len(instance.get_elements()) < len(features)
+        - First, find what is missing
+        - Second, parse normally using `parse_values`
+        - Thrid, insert None into missing values
+        """
+        missing = []
+        for element in instance.get_elements("FeatureValues")[0].x_key:
+            ref = element.get_attribute("x:idref")
+            missing.append([ref in self.categories[user][f] for f in self.features])
+        missing = np.array(missing)
+        missing_index = np.argwhere(np.all(~missing, axis=0) == True).flatten().tolist()
+        values = self._parse_values(user, instance)
+        for index in missing_index:
+            values.insert(index, None)
 
-    def _parse_list_values():
-        pass
+        return values
+
+    def _parse_list_values(self, user: str, instance: Element) -> list:
+        values = []
+        refs = [
+            element.get_attribute("x:idref")
+            for element in instance.get_elements("FeatureValues")[0].x_key
+        ]
+        doubles = [
+            float(element.cdata)
+            for element in instance.get_elements("FeatureValues")[0].Double
+        ]
+        features = self.features[:]
+        it_f = peekable(features)
+        it_d = peekable(doubles)
+        multiples = []
+        for feature, ref, double in zip_longest(it_f, refs, it_d):
+            if not self._check_pos_float(double):
+                values.append(self._get_value(user, feature, ref, int(double)))
+                continue
+
+            if self._check_pos_float(double):
+                multiples.append(self._get_value(user, feature, ref, double))
+                if self._check_pos_float(it_d.peek(1)):
+                    it_f.prepend(feature)
+                else:
+                    values.append(";".join(multiples))
+                    multiples = []
+
+        return values
+
+    def _check_pos_float(self, num: float) -> bool:
+        return num < 1 and num > 0
 
     def to_pandas(self):
         data = []
@@ -98,8 +159,19 @@ class FeatureSet:
                     repliedTo = True if instance.get_attribute("Label") else False
                     row = []
                     row.extend([user, dataset])
-                    tmp = self._parse_values(user, instance)
-                    row.extend(tmp)
+                    if len(self.features) * 2 == len(
+                        instance.get_elements("FeatureValues")[0]
+                    ):
+                        values = self._parse_values(user, instance)
+                    elif len(self.features) * 2 > len(
+                        instance.get_elements("FeatureValues")[0]
+                    ):
+                        values = self._parse_missing_values(user, instance)
+                    elif len(self.features) * 2 < len(
+                        instance.get_elements("FeatureValues")[0]
+                    ):
+                        values = self._parse_list_values(user, instance)
+                    row.extend(values)
                     row.append(repliedTo)
                     row = tuple(row)
                     data.append(row)
